7090a0e90fa4ce17e8f4038d3106c7d8ed648936
mirza irtiza <mirzairtiza51@gmail.com>
2024-05-27 17:29:25 +0500

    structure update

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
 // App.tsx
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import WebsiteHome from './website/pages';
-import ProductDetail from './website/pages/Detail';
-import Cart from './website/pages/Cart';
-import Checkout from './website/pages/Checkout';
-import Favorites from './website/pages/Favorites';
-import NotFound from './dashboard/pages/NotFound';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import ProductDetail from "./pages/Detail";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Favorites from "./pages/Favorites";
+import NotFound from "./pages/NotFound";
 
 // dashboard
-import DashboardHome from './dashboard/pages';
-import DashboardLogin from './dashboard/pages/Login';
+// import DashboardHome from './dashboard/pages';
+// import DashboardLogin from './dashboard/pages/Login';
 
 const App: React.FC = () => {
   // const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false); // Assuming you have a state to track login status
-  
+
   return (
     <Router>
       <Routes>
         {/* Website Routes */}
-        <Route path="/" element={<WebsiteHome />} />
+        <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/favorites" element={<Favorites />} />
-        
+
         {/* Protected Dashboard Route */}
-        <Route path="/dashboard/*" element={<DashboardRoutes />} />
-        
+        {/* <Route path="/dashboard/*" element={<DashboardRoutes />} /> */}
+
         {/* 404 Not Found */}
         <Route path="*" element={<NotFound />} />
       </Routes>
@@ -37,18 +37,18 @@ const App: React.FC = () => {
 };
 
 // Define a separate component for dashboard routes
-const DashboardRoutes: React.FC = () => {
-  // console.log(isLoggedIn)
-  // if (!isLoggedIn) {
-  //   return <Navigate to="/dashboard/login" />    ;
-  // }
+// const DashboardRoutes: React.FC = () => {
+// console.log(isLoggedIn)
+// if (!isLoggedIn) {
+//   return <Navigate to="/dashboard/login" />    ;
+// }
 
-  return (
-    <Routes>
-      <Route path="/" element={<DashboardHome />} />
-      <Route path="/login" element={<DashboardLogin />} />
-    </Routes>
-  );
-};
+//   return (
+//     <Routes>
+//       <Route path="/" element={<DashboardHome />} />
+//       <Route path="/login" element={<DashboardLogin />} />
+//     </Routes>
+//   );
+// };
 
 export default App;
